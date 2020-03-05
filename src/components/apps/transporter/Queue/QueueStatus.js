ab2bdb9e98aec55b15b288a69011a3d92586d0cd
nzepa74 <nzepa.ttpl@gmail.com>
2020-03-05 15:12:40 +0600

    queue status

--- src/components/apps/transporter/Queue/QueueStatus.js
+++ src/components/apps/transporter/Queue/QueueStatus.js
@@ -1,29 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import { View, Image, Dimensions, TouchableHighlight, } from 'react-native';
+import { View, Dimensions, TouchableHighlight, } from 'react-native';
 
 import {
     Container,
-    Input,
     Content,
     Form,
-    Picker,
-    Item,
-    Button,
     Text,
-    Card,
-    CardItem,
-    Icon,
-    DeckSwiper,
-    Modal
+    CardItem
 } from 'native-base';
 import {
-    callAxios,
-    handleError,
-    getImages,
     setLoading,
 } from '../../../../redux/actions/commonActions';
-import { startTransportRegistration } from '../../../../redux/actions/transportActions';
 import globalStyles from '../../../../styles/globalStyle';
 import SpinnerScreen from '../../../base/SpinnerScreen';
 import BootstrapStyleSheet from 'react-native-bootstrap-styles';
@@ -33,27 +21,11 @@ export const QueueStatus = ({
     userState,
     commonState,
     navigation,
-    startTransportRegistration,
-    handleError,
-    getImages,
     setLoading,
 }) => {
 
     const bootstrapStyleSheet = new BootstrapStyleSheet();
     const s = bootstrapStyleSheet.create();
-    // const c = constants = bootstrapStyleSheet.constants;
-
-    //state info for forms
-    const [vehicle_no, setVehicle_no] = useState('');
-    const [vehicle_capacity, setVehicle_capacity] = useState(undefined);
-    const [drivers_name, setdrivers_name] = useState(undefined);
-    const [contact_no, setcontact_no] = useState(undefined);
-    const [registration_document, setregistration_document] = useState([]);
-    const [images, setImages] = useState([]);
-
-    //all values
-    const [all_capacities, setall_capacities] = useState([]);
-
     //For proper navigation/auth settings
     useEffect(() => {
         if (!userState.logged_in) {
@@ -61,51 +33,10 @@ export const QueueStatus = ({
         } else if (!userState.profile_verified) {
             navigation.navigate('UserDetail');
         } else {
-            //get all capacities
-            setLoading(true);
-            getCapacities();
+            // setLoading(true);
+            setLoading(false);
         }
     }, []);
-
-    useEffect(() => {
-        setImages([]);
-        setTimeout(() => {
-            setImages(registration_document);
-        }, 600);
-    }, [registration_document]);
-
-    //image picker
-    const getBluebook = async () => {
-        const bluebooks = await getImages('Bluebook');
-        setregistration_document(bluebooks);
-    };
-
-    const removeImage = () => {
-        setregistration_document(images.filter((_, ind) => ind > 0));
-    };
-
-    const getCapacities = async () => {
-        try {
-            const all_st = await callAxios('resource/Vehicle Capacity');
-            setall_capacities(all_st.data.data);
-            setLoading(false);
-        } catch (error) {
-            handleError(error);
-        }
-    };
-    const submitVehicleInfo = async () => {
-        const vehicle_info = {
-            approval_status: 'Pending',
-            user: userState.login_id,
-            common_pool: 1,
-            vehicle_no: vehicle_no.toUpperCase(),
-            vehicle_capacity,
-            drivers_name,
-            contact_no,
-            owner_cid: userState.login_id,
-        };
-        startTransportRegistration(vehicle_info, registration_document);
-    };
 
     return commonState.isLoading ? (
         <SpinnerScreen />
@@ -114,6 +45,13 @@ export const QueueStatus = ({
                 <Content style={globalStyles.content}>
                     <Form>
                         <View style={[s.card]}>
+                            <CardItem>
+                                <Text style={[s.h5, s.textSecondary]}>
+                                    Vehicle No: BP-A-2020</Text>
+                                <Text style={[s.h5, s.textSecondary]}>
+                                    {'  '} Capacity: 10 M3</Text>
+                            </CardItem>
+
                             <TouchableHighlight
                                 style={{
                                     borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
@@ -124,12 +62,12 @@ export const QueueStatus = ({
                                     alignItems: 'center',
                                     marginLeft: 140
                                 }}
-                                onPress={() => alert('Hello')}
+                                // onPress={() => alert('Hello')}
                                 underlayColor='#ccc'>
-                                <Text style={{ fontSize: 25, fontWeight: "bold", color: "white" }}>10</Text>
+                                <Text style={{ fontSize: 25, fontWeight: "bold", color: "white" }}>12</Text>
                             </TouchableHighlight>
-                            <Text style={[s.text, s.textCenter, s.h4, s.textSecondary, s.myXs1, s.myMd3]}>
-                                Truck ahead of you
+                            <Text style={[s.text, s.textCenter, s.h5, s.textSecondary, s.myXs1, s.myMd3]}>
+                                Truck(s) ahead of you
                             </Text>
                             <TouchableHighlight
                                 style={{
@@ -144,7 +82,7 @@ export const QueueStatus = ({
                                 underlayColor='#ccc'>
                                 <Text style={{ fontSize: 40, fontWeight: "bold", color: "white" }}>11</Text>
                             </TouchableHighlight>
-                            <Text style={[s.text, s.textCenter, s.h4, s.textSecondary, s.myXs1, s.myMd3]}>
+                            <Text style={[s.text, s.textCenter, s.h5, s.textSecondary, s.myXs1, s.myMd3]}>
                                 Your position
                             </Text>
 
@@ -161,22 +99,16 @@ export const QueueStatus = ({
                                 underlayColor='#ccc'>
                                 <Text style={{ fontSize: 25, fontWeight: "bold", color: "white" }}>10</Text>
                             </TouchableHighlight>
-                            <Text style={[s.text, s.textCenter, s.h4, s.textSecondary, s.myXs1, s.myMd3]}>
-                                Truck behind you
+                            <Text style={[s.text, s.textCenter, s.h5, s.textSecondary, s.myXs1, s.myMd3]}>
+                                Truck(s) behind you
                             </Text>
                         </View>
-
-                        <Text style={[s.text, s.textCenter, s.h3, s.textSecondary, s.myXs1, s.myMd3]}>
+                        <Text style={[s.text, s.textCenter, s.h4, s.textSecondary, s.myXs1, s.myMd3]}>
                             Thank you for waiting
                             </Text>
-
-
-
                     </Form>
-
-
                 </Content>
-            </Container >
+             </Container >
         );
 };
 
@@ -186,9 +118,6 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = {
-    startTransportRegistration,
-    handleError,
-    getImages,
     setLoading,
 };
 
